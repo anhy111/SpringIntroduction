6d897898fc4a24a87d71950f83877230ad2f6972
anhy111 <gkdyd123@gmail.com>
2023-06-13 21:41:14 +0900

    second commit

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
-
+    MemoryMemberRepository abc = new MemoryMemberRepository();
     @AfterEach
     public void afterEach() {
         repository.clearStore();
